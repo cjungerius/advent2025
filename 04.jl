e243dc01b923bbadcae4c490998b38acb1c1b6f0
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2025-12-04 10:38:51 +0000

    optimize day 4 with matrix and frontier

--- 04.jl
+++ 04.jl
@@ -1,39 +1,44 @@
-function find_rolls(input)
+function find_rolls(input, frontier)
         roll_count = 0
         removable = Tuple{Int,Int}[]
-        y_max = length(input)
-        x_max = length(input[1])
+        new_frontier = Set{Tuple{Int,Int}}()
+        (y_max, x_max) = size(input)
         surround = filter(x -> x != (0, 0), [(i, j) for i in -1:1 for j in -1:1])
-        for y in 1:y_max
-                for x in 1:x_max
-                        input[y][x] == "." && continue
-                        rolls = 0
-                        for s in surround
-                                y_s = y + s[1]
-                                x_s = x + s[2]
-                                !(1 <= y_s <= y_max) && continue
-                                !(1 <= x_s <= x_max) && continue
-                                rolls += input[y_s][x_s] == "@"
-                        end
-                        if rolls < 4
-                                roll_count += 1
-                                push!(removable, (y, x))
+        potential_frontier = falses(8)
+        y_s, x_s = (0, 0)
+        for (y, x) in frontier
+                input[y, x] == '.' && continue
+                potential_frontier = falses(8)
+                for (i, s) in enumerate(surround)
+                        y_s = y + s[1]
+                        x_s = x + s[2]
+                        !(1 <= y_s <= y_max) && continue
+                        !(1 <= x_s <= x_max) && continue
+                        potential_frontier[i] = input[y_s, x_s] == '@'
+                end
+                if sum(potential_frontier) < 4
+                        roll_count += 1
+                        push!(removable, (y, x))
+                        for i in eachindex(potential_frontier)
+                                !potential_frontier[i] && continue
+                                push!(new_frontier, (y, x) .+ surround[i])
                         end
                 end
         end
-        (roll_count, removable)
+        (roll_count, removable, new_frontier)
 end
 
 function solve(input)
-        input = split.(input, "")
-        roll_count, removable = find_rolls(input)
+        input = stack(collect.(input), dims=1)
+        frontier = Set{Tuple{Int,Int}}([(y, x) for y in 1:size(input)[1] for x in 1:size(input)[2]])
+        roll_count, removable, frontier = find_rolls(input, frontier)
         part_1 = roll_count
         part_2 = roll_count
         while length(removable) > 0
                 for r in removable
-                        input[r[1]][r[2]] = "."
+                        input[r[1], r[2]] = '.'
                 end
-                roll_count, removable = find_rolls(input)
+                roll_count, removable, frontier = find_rolls(input, frontier)
                 part_2 += roll_count
         end
         (part_1, part_2)
