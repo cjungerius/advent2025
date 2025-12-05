f6e1f044fe266cd9bdbb0cb6d746f747852e1e15
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2025-12-05 05:37:54 +0000

    solve day 5

---
+++ 05.jl
@@ -0,0 +1,50 @@
+function solve(input)
+        fresh_ranges = Vector{Int}[]
+        ingredients = Int[]
+        flip = false
+        for line in input
+                if line == ""
+                        flip = true
+                        continue
+                end
+
+                if flip
+                        push!(ingredients, parse(Int, line))
+                else
+                        push!(fresh_ranges, parse.(Int, split(line, "-")))
+                end
+        end
+
+        sort!(fresh_ranges, by=x -> x[1])
+        fresh_ranges_merged = Vector{Int}[]
+        push!(fresh_ranges_merged, fresh_ranges[1])
+
+        for r in fresh_ranges[2:end]
+                old_lower, old_upper = fresh_ranges_merged[end]
+                lower, upper = r
+                if lower > old_upper + 1
+                        push!(fresh_ranges_merged, r)
+                else
+                        fresh_ranges_merged[end][2] = max(old_upper, upper)
+                end
+        end
+
+        part_one = 0
+        for ingredient in ingredients
+                for r in fresh_ranges_merged
+                        if ingredient < r[1]
+                                break
+                        elseif ingredient > r[2]
+                                continue
+                        else
+                                part_one += 1
+                                break
+                        end
+                end
+        end
+
+        part_two = sum([r[2] - r[1] + 1 for r in fresh_ranges_merged])
+
+
+        part_one, part_two
+end
