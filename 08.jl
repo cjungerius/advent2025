6c2fb9f5c3dfc6a7253b623919e5b1bb45f0d95c
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2025-12-08 07:29:13 +0000

    optimize day 8 with own priority queue

--- 08.jl
+++ 08.jl
@@ -3,15 +3,16 @@ using StatsBase
 
 function solve(input)
         points = [parse.(Int, split(p, ',')) for p in input]
-        pq = PriorityQueue{Tuple{Int,Int},Float64}()
+        pq = Pair{Tuple{Int,Int},Int}[]
 
         for j in 1:length(points)
                 for i in 1:j-1
-                        d = sqrt(sum((points[j] .- points[i]) .^ 2))
-                        pq[(i, j)] = d
+                        d = sum((points[j] .- points[i]) .^ 2)
+                        push!(pq, (i, j) => d)
                 end
         end
 
+        sort!(pq, by=last)
         circuits = IntDisjointSet(length(points))
         cycles = 0
         part_one = 0
@@ -19,13 +20,13 @@ function solve(input)
         j = 0
 
         while num_groups(circuits) > 1
-                (i, j) = dequeue!(pq)
+                (i, j) = first(popfirst!(pq))
                 union!(circuits, i, j)
                 cycles += 1
                 if cycles == 1000
                         roots = find_root.(Ref(circuits), 1:length(points))
-                        sizes = sort!(counts(roots))
-                        part_one = *(sizes[end-2:end]...)
+                        sizes = sort!(counts(roots), rev=true)
+                        part_one = *(sizes[1:3]...)
                 end
         end
         part_two = points[i][1] * points[j][1]
