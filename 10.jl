086a58d954d23fbfdf791a3440f8b261c065008a
Chris Jungerius <37599089+cjungerius@users.noreply.github.com>
2025-12-10 20:26:20 +0000

    solve day 10 part 2. Learned JuMP! :)

--- 10.jl
+++ 10.jl
@@ -1,13 +1,6 @@
 using DataStructures
-using Memoize
-
-function press_buttons(lights::BitVector, button::Union{Tuple{Int,Vararg{Int}},Int})
-        new_lights = copy(lights)
-        for b in button
-                new_lights[b+1] = !new_lights[b+1]
-        end
-        return new_lights
-end
+using JuMP
+using HiGHS
 
 function solve(input)
         presses = 0
@@ -19,7 +12,8 @@ function solve(input)
                 buttons = pattern_matches[i].captures[2]
                 buttons = [m.captures[1] for m in eachmatch(button_pattern, buttons)]
                 buttons = split.(buttons, ",")
-                buttons = [Tuple(parse.(Int, b)) for b in buttons]
+                buttons = [parse.(Int, b) for b in buttons]
+                buttons = [BitVector([a in b for a in 0:length(target)-1]) for b in buttons]
 
                 lights = falses(length(target))
                 q = Queue{Tuple}()
@@ -35,7 +29,7 @@ function solve(input)
                         lights, b, steps = dequeue!(q)
                         lights == target && break
                         for b in buttons
-                                next_lights = press_buttons(lights, b)
+                                next_lights = lights .⊻ b
                                 if !(next_lights in seen)
                                         push!(seen, next_lights)
                                         enqueue!(q, (next_lights, b, steps + 1))
@@ -45,4 +39,43 @@ function solve(input)
                 presses += steps
         end
         presses
+end
+
+function solve2(input)
+        pattern = r"\[(.+)\] (.+)\ \{(.+)\}"
+        button_pattern = r"\(([0-9,]+)\)"
+        pattern_matches = match.(pattern, input)
+        data = Tuple[]
+        for i in eachindex(pattern_matches)
+                b = parse.(Int, split(pattern_matches[i].captures[3], ","))
+                buttons = pattern_matches[i].captures[2]
+                buttons = [m.captures[1] for m in eachmatch(button_pattern, buttons)]
+                buttons = split.(buttons, ",")
+                buttons = [Tuple(parse.(Int, b)) for b in buttons]
+                A = zeros(Int, length(b), length(buttons))
+                for button in eachindex(buttons)
+                        A[:, button] = [c in buttons[button] for c in 0:length(b)-1]
+                end
+                push!(data, (A, b))
+        end
+
+        model = direct_model(HiGHS.Optimizer())
+        set_silent(model)
+
+        total_vars = sum(size(d[1], 2) for d in data)
+
+        @variable(model, x[1:total_vars] >= 0, Int)
+
+        var_ptr = 1
+
+        for (A, b) in data
+                rows, cols = size(A)
+                current_vars = x[var_ptr:var_ptr+cols-1]
+                @constraint(model, A * current_vars .== b)
+                var_ptr += cols
+        end
+
+        @objective(model, Min, sum(x))
+        optimize!(model)
+        return Int(sum(value.(x)))
 end
